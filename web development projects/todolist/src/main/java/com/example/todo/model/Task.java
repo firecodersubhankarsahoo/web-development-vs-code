096d30514e1794f7124519d559f527bbb7a86beb
java code <sahoosubhankar98325@gmail.com>
2024-06-17 11:36:53 +0530

    l

---
+++ web development projects/todolist/src/main/java/com/example/todo/model/Task.java
@@ -0,0 +1,10 @@
+package com.example.todo.todolist.model;
+
+public class Task {
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    // Getters and setters
+    // Constructors
+}
