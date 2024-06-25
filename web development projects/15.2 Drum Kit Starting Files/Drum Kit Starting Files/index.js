0d456c43034903f95f9a15324e63e495901a8785
java code <sahoosubhankar98325@gmail.com>
2024-06-26 00:20:10 +0530

    ok

--- web development projects/15.2 Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ web development projects/15.2 Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -5,5 +5,46 @@ document.querySelectorAll(".drum")[i].addEventListener("click",handle);
 }
 function handle(){
 
-alert("i got clickrd");
+    var buttonName=this.innerHTML;
+    switch(buttonName){
+        case "w":
+                playSound("./sounds/crash.mp3");
+                break;
+        case "a":
+            playSound("./sounds/tom-4.mp3");
+                break;
+        case "s":
+            playSound("./sounds/snare.mp3");
+                break;
+        case "d":
+            playSound("./sounds/tom-1.mp3");
+                break;
+        case "j":
+            playSound("./sounds/tom-2.mp3");
+                break;
+        case "k":
+            playSound("./sounds/tom-3.mp3");
+                break;
+        case "l":
+            playSound("./sounds/kick-bass.mp3");
+                break;
+        default:
+            console.log("invalid button");
+
+
+
+    }
+
+
+function playSound(location){
+var sound=new Audio(location);
+    sound.play();
 }
+
+
+
+
+    
+
+}
+
